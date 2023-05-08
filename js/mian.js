222253c307b8fc434b81328b7e01010f0e56f664
Doplax <doplax@gmail.com>
2023-05-08 08:04:29 +0200

    build: He vuelto a poner el footer en su sitio

--- js/mian.js
+++ js/mian.js
@@ -1,21 +1,22 @@
+// FUNCIONES
 // Función para manejar el evento de clic en el botón de menú
-function handleMenuToggle() {
-    navMenu.classList.toggle("nav-menu_visible");
+//function handleMenuToggle() {
+//    navMenu.classList.toggle("nav-menu_visible");
 
-    if (navMenu.classList.contains("nav-menu_visible")) {
-        navToggle.setAttribute("aria-label", "Cerrar menú");
-    } else {
-        navToggle.setAttribute("aria-label", "Abrir menú");
-    }
-}
+//    if (navMenu.classList.contains("nav-menu_visible")) {
+//        navToggle.setAttribute("aria-label", "Cerrar menú");
+//    } else {
+//        navToggle.setAttribute("aria-label", "Abrir menú");
+//    }
+//}
 
 // Función para cerrar el menú
-function closeMenu() {
-    navMenu.classList.remove("nav-menu_visible");
-}
+//function closeMenu() {
+//    navMenu.classList.remove("nav-menu_visible");
+//}
 
-// Función para manejar el evento de desplazamiento en la ventana
-function handleScroll() {
+// Función para manejar la animación del menú
+function scrollMenu() {
     const header = document.getElementById("header");
     const navLogo = document.querySelector('.nav__logo')
 
@@ -25,72 +26,51 @@ function handleScroll() {
         //navLogo.src = '../img/company_logo_black.png'
     } else {
         header.classList.remove("header__scroll");
-
     }
+}
+
+function nextSection() {
+    // Cuando hacemos scroll y nos quedamos a mitad de camino, esto nos ayuda a centrar la sección
+    if (window.innerWidth > 1100) {
+        const sections = document.querySelectorAll(".section");
+        let scrollTimeout;
     
+        window.addEventListener("scroll", () => {
+        clearTimeout(scrollTimeout);
     
+        scrollTimeout = setTimeout(() => {
+            let closestSection = null;
+            let minDistance = Number.MAX_VALUE;
+    
+            sections.forEach((section) => {
+            const distanceFromTop = Math.abs(section.getBoundingClientRect().top);
+            if (distanceFromTop < minDistance) {
+                minDistance = distanceFromTop;
+                closestSection = section;
+            }
+            });
+    
+            window.scrollTo({
+            top: closestSection.offsetTop,
+            behavior: "smooth",
+            });
+        }, 100);
+        });
+    }
 }
 
 
 
-//function renderSkills(skills) {
-//    const skillsContainer = document.querySelector(".skill__list");
-
-//    skills.forEach((skill) => {
-//        const div = document.createElement('div');
-//        div.innerHTML = `
-//        <div class="skill__item">
-//            <img src="${skill.icon}" alt="${skill.altText}">
-//            <div class="skill__name">${skill.skillName}</div>
-//        </div>`;
-//    skillsContainer.appendChild(div)
-//    });
-
-//}
-
-
-
+// VARIABLES
 const navToggle = document.querySelector(".nav-toggle");
 const navMenuItem = document.querySelector(".nav-menu");
-const check = document.querySelector(".check");
 
 
-const header = document.querySelector('#header');
-
-// Registra los controladores de eventos
-navToggle.addEventListener("click", handleMenuToggle);
-navMenuItem.addEventListener("click", closeMenu);
-//check.addEventListener("click", handleLanguage);
-window.addEventListener("scroll", handleScroll);
+//navToggle.addEventListener("click", handleMenuToggle);
+//navMenuItem.addEventListener("click", closeMenu);
+document.addEventListener("DOMContentLoaded", nextSection);
 
 
-//document.addEventListener("DOMContentLoaded", () => {
-//    const sections = document.querySelectorAll(".section");
-//    let scrollTimeout;
-  
-//    window.addEventListener("scroll", () => {
-//      clearTimeout(scrollTimeout);
-  
-//      scrollTimeout = setTimeout(() => {
-//        let closestSection = null;
-//        let minDistance = Number.MAX_VALUE;
-  
-//        sections.forEach((section) => {
-//          const distanceFromTop = Math.abs(section.getBoundingClientRect().top);
-//          if (distanceFromTop < minDistance) {
-//            minDistance = distanceFromTop;
-//            closestSection = section;
-//          }
-//        });
-  
-//        window.scrollTo({
-//          top: closestSection.offsetTop,
-//          behavior: "smooth",
-//        });
-//      }, 100);
-//    });
-//  });
-  
+window.addEventListener("scroll", ()=> scrollMenu());
 
 
-  
